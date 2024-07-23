e13d7e76d6f0b91c89374549ec8e6101e5fd1052
Zakhar Polikarpov <zpolikarpov@veka.de>
2024-07-23 15:21:57 +0200

    Mobile header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import icon_logo from "./assets/images/logo.svg"
+
 function App() {
 	return (
 		<>
-			<h1 className="heading-1">Hello World</h1>
+      <div className="container">
+        <header className="site-header">
+            <div className="site-header__inner">
+              <img className="logo" src={icon_logo} alt="Rock Paper Scissors" />
+              <div className="scoreboard">
+                <p className="heading-3" style={{letterSpacing: "1.58px"}}>SCORE</p>
+                <p className="fs-1000 clr-neutral-400">10</p>
+              </div>
+            </div>
+        </header>
+      </div>
 		</>
   )
 }
